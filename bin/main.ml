970f891b015897257c3a109bc28da543a4f6a187
GiGi Hurtault <gigi@hurtault.dev>
2025-09-14 01:22:05 -0400

    add card count

--- bin/main.ml
+++ bin/main.ml
@@ -2,4 +2,4 @@ open! Core
 
 let () =
 Blackjack.Deck.full_deck
-|> List.iter ~f:(fun card -> print_s (Blackjack.Card.sexp_of_t card))
+|> List.iter ~f:(fun card -> print_s (Blackjack.Card.sexp_of_t card)); let count = List.length Blackjack.Deck.full_deck in print_endline [%string "there are %{count#Int} cards in the full_deck value"]
